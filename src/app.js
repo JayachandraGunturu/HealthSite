89e72c98140a057b2952d8f9ede56c5648960220
jayachandra gunturu <gunturujayachandra@gmail.com>
2020-07-24 14:59:56 +0530

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const viewsPath = path.join(__dirname,"./templates/views");
+const viewsPath = path.join(__dirname,"../templates/views");
 
 
 app.set('view engine', 'hbs');
