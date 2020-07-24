53cb2aeedf4db6640d010ce60a4f543fd4f316cb
jayachandra gunturu <gunturujayachandra@gmail.com>
2020-07-24 14:22:17 +0530

    Init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+const viewsPath = path.join(__dirname,"./templates/views");
+
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.listen(port,()=>{
+    console.log("listening on port",port);
+});
+
